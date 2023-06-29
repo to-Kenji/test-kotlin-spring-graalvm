623c729bd57b528445d4e18fd49c4ad437fb1585
to-Kenji <kura101089kenji@gmail.com>
2023-06-30 08:16:53 +0900

    add: post controller

--- src/main/kotlin/com/example/testkotlinspringgraalvm/MyController.kt
+++ src/main/kotlin/com/example/testkotlinspringgraalvm/MyController.kt
@@ -1,10 +1,21 @@
 package com.example.testkotlinspringgraalvm
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @RestController
 class MyController {
+
+    @PostMapping("/post")
+    fun post(@RequestBody request: String): String {
+
+        val restTemplate = RestTemplate()
+
+        return restTemplate.postForObject("https://httpbin.org/post", request, String::class.java)!!
+    }
 
     @GetMapping("/health")
     fun healthCheck(): String {
