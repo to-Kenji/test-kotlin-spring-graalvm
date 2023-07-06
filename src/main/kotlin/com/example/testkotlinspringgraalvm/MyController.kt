924f682ec8e76a784fb2cf29cec06e7b328ab966
to-Kenji <kura101089kenji@gmail.com>
2023-07-07 07:27:29 +0900

    wip: @RegisterReflectionForBinding

--- src/main/kotlin/com/example/testkotlinspringgraalvm/MyController.kt
+++ src/main/kotlin/com/example/testkotlinspringgraalvm/MyController.kt
@@ -1,5 +1,11 @@
 package com.example.testkotlinspringgraalvm
 
+import org.springframework.aot.hint.MemberCategory
+import org.springframework.aot.hint.RuntimeHints
+import org.springframework.aot.hint.RuntimeHintsRegistrar
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.ImportRuntimeHints
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,10 +21,7 @@ class MyController {
         println("-------------------------------------------MyController-------------------------------------------------")
         val restTemplate = RestTemplate()
 
-        val request = MyApiRequest()
-        request.requestBody = req.str
-        request.task = "task1"
-
+        val request = MyApiRequest(req.str, "test")
         println("generated api request: $request")
         val response: String;
         try {
@@ -36,12 +39,17 @@ class MyController {
 }
 
 data class MyRequest(val str: String)
-class MyApiRequest{
-    var requestBody: String = "initial"
-    var task: String = ""
-    override fun toString(): String {
-        return "MyApiRequest(requestBody='$requestBody', task='$task')"
-    }
-}
+data class MyApiRequest(val requestBody: String, val task: String)
 
 // data class MyChildApiRequest(val title: String, val description: String)
+
+@Configuration
+@RegisterReflectionForBinding(RestTemplate::class)
+@ImportRuntimeHints(MyConfiguration.AppRunTimeHintsRegister::class)
+class MyConfiguration {
+    class AppRunTimeHintsRegister : RuntimeHintsRegistrar {
+        override fun registerHints(hints: RuntimeHints, classLoader: ClassLoader?) {
+            hints.reflection().registerType(RestTemplate::class.java, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.DECLARED_CLASSES)
+        }
+    }
+}
