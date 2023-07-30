540001fc09fc6231a7acbe9063bd21e68af6f635
to-Kenji <kura101089kenji@gmail.com>
2023-07-31 07:38:31 +0900

    wip: registerReflectionForBindingだけ入れておく

--- src/main/kotlin/com/example/testkotlinspringgraalvm/MyController.kt
+++ src/main/kotlin/com/example/testkotlinspringgraalvm/MyController.kt
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 
 @RestController
+@RegisterReflectionForBinding(MyRequest::class)
 class MyController {
 
     @PostMapping("/post")
